9a37cec5ed56ba86a2c7e325a3cb3854c6811d59
Viktor Kopan <victorkopan98@gmail.com>
2023-07-11 14:56:24 +0600

    AboutSection

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import Title from '@/components/Title/Title';
 import styles from './AboutSection.module.css';
 import Papich from '@/assets/papich.jpg';
+import FireIcon from './fire.svg?svgr';
 
 const skills = [
   'JavaScript (ES6+)',
@@ -16,12 +17,12 @@ const skills = [
 
 const AboutSection = () => {
   return (
-    <section className="py-25 px-10">
+    <section className="px-10 py-24">
       <Title tag="h2" className="mb-10">
         Обо Мне
       </Title>
       <div className="grid gap-12 grid-cols-5">
-        <div className="col-span-3">
+        <div className="col-span-3 border-l-4 border-cyclamen pl-3">
           <p className="mb-5">
             Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев более менее
             осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык публичных выступлений в
@@ -32,12 +33,20 @@ const AboutSection = () => {
           <p className="mb-5">Вот технологии с которыми я работаю:</p>
           <ul className={styles.skillsList}>
             {skills.map((skill) => (
-              <li key={skill}>{skill}</li>
+              <li key={skill} className="flex items-center gap-2">
+                <FireIcon className="inline-block fill-cyclamen" width={13} height={13} /> {skill}
+              </li>
             ))}
           </ul>
         </div>
         <div className="col-span-2">
-          <Image src={Papich} alt="Roflan ebalo" />
+          <div className="bg-cyclamen rounded">
+            <Image
+              src={Papich}
+              alt="Roflan ebalo"
+              className="rounded transition-transform hover:-translate-x-2 hover:-translate-y-2"
+            />
+          </div>
         </div>
       </div>
     </section>
